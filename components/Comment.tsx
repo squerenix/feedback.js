6dffc6eb483bc98bf84e106116ffda8a2b92981e
Vizucode <havisikkubaru@gmail.com>
2022-02-20 13:00:09 +0700

    fix: responsive views

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -4,29 +4,29 @@ import {NextComponentType} from 'next';
 const Comment: NextComponentType = () => {
     return (
         <>
-            <div className="masonry before:box-inherit after:box-inherit gap-5 pt-20 pb-10">
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 hover:scale-105 duration-500 cursor-pointer tracking-wide">
+            <div className="xl:masonry before:box-inherit after:box-inherit gap-5 py-10 px-5">
+                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                 </div>
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 hover:scale-105 duration-500 cursor-pointer tracking-wide">
+                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                 </div>
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 hover:scale-105 duration-500 cursor-pointer tracking-wide">
+                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                 </div>
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 hover:scale-105 duration-500 cursor-pointer tracking-wide">
+                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                 </div>
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 hover:scale-105 duration-500 cursor-pointer tracking-wide">
+                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                 </div>
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 hover:scale-105 duration-500 cursor-pointer tracking-wide">
+                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
                 </div>
             </div>
