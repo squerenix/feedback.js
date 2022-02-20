2400e88124021f3f3ee6273d90e5c077d39a6e0a
Vizucode <havisikkubaru@gmail.com>
2022-02-20 19:23:46 +0700

    feat: add timestamp comments

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -5,29 +5,38 @@ const Comment: NextComponentType = () => {
     return (
         <>
             <div className="xl:masonry before:box-inherit after:box-inherit gap-5 py-10 px-5">
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
+                <div className="break-inside py-4 px-3 mb-5 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    <span className="text-sm block mt-5 text-gray-400">8 day ago</span>
                 </div>
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
+                <div className="break-inside py-4 px-3 mb-5 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    <span className="text-sm block mt-5 text-gray-400">8 day ago</span>
                 </div>
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
+                <div className="break-inside py-4 px-3 mb-5 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    <span className="text-sm block mt-5 text-gray-400">8 day ago</span>
                 </div>
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
+                <div className="break-inside py-4 px-3 mb-5 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    <span className="text-sm block mt-5 text-gray-400">8 day ago</span>
                 </div>
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
-                </div>
-                <div className="break-inside py-4 px-3 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum consectetur ducimus quisquam ea. Eveniet est asperiores nihil necessitatibus fugit expedita atque architecto cum ipsum veritatis? Dolor a deserunt nulla amet!
+                <div className="break-inside py-4 px-3 mb-5 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
+                    <span className="text-sm block mt-5 text-gray-400">8 day ago</span>
                 </div>
             </div>
         </>
