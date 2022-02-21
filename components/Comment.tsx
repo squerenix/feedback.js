6120635049f0d331c5e61859ca870833967f6a81
Vizucode <havisikkubaru@gmail.com>
2022-02-21 11:20:56 +0700

    feat: read data with firebase

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,43 +1,41 @@
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import moment from 'moment';
 import {NextComponentType} from 'next';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase/initFirebase';
 
 
 const Comment: NextComponentType = () => {
+
+    interface Icomment {
+        text: string,
+        timestamp: any
+    }
+
+    const [comments, setComment] = useState<Array<Icomment>>([]);
+
+    // read data
+    useEffect(() => {
+        const ref = collection(db, "feedback");
+        const q   = query(ref);
+        
+        onSnapshot(q, (QuerySnapshot) => {
+            setComment(QuerySnapshot.docs.map(doc => ({
+                text: doc.data().text, timestamp: doc.data().timestamp?.toDate().getTime()
+            })));
+        })
+
+    }, []);
+
     return (
         <>
             <div className="xl:masonry before:box-inherit after:box-inherit gap-5 py-10 px-5">
-                <div className="break-inside py-4 px-3 mb-5 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    <span className="text-sm block mt-5 text-gray-400">8 day ago</span>
-                </div>
-                <div className="break-inside py-4 px-3 mb-5 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    <span className="text-sm block mt-5 text-gray-400">8 day ago</span>
-                </div>
-                <div className="break-inside py-4 px-3 mb-5 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    <span className="text-sm block mt-5 text-gray-400">8 day ago</span>
-                </div>
-                <div className="break-inside py-4 px-3 mb-5 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    <span className="text-sm block mt-5 text-gray-400">8 day ago</span>
-                </div>
-                <div className="break-inside py-4 px-3 mb-5 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium autem repellat quod ipsum, optio nulla fuga voluptatibus consequuntur cupiditate ducimus eius. Provident repellat soluta nisi qui esse aliquid mollitia.
-                    <span className="text-sm block mt-5 text-gray-400">8 day ago</span>
-                </div>
+                {comments.map(comment => (
+                    <div key={comment.timestamp} className="break-inside py-4 px-3 mb-5 bg-gray-300 rounded text-md font-medium font-rubik text-slate-600 shadow-lg hover:shadow-gray-400 xl:hover:scale-105 duration-500 cursor-pointer tracking-wide">
+                        { comment.text }
+                        <span className="text-sm block mt-5 text-gray-400">{moment(comment.timestamp).fromNow()}</span>
+                    </div>
+                ))}
             </div>
         </>
     );
